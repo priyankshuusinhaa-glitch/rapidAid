bb4d6323777e8042bf927e8a042923a0ed1cabb6
Jigyansu Mishra <114947954+JIGYANSU-MISHRA@users.noreply.github.com>
2025-08-09 21:14:24 +0530

    Delete Admin-panel directory

--- Admin-panel/src/config/roles.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  roles: {
-    SUPERADMIN: 'SuperAdmin',
-    MANAGER: 'Manager',
-    HELPLINE_OPERATOR: 'HelplineOperator',
-    DISPATCHER: 'Dispatcher',
-    USER: 'User',
-  },
-};
